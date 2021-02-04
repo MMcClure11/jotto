e37f3a494c7ad210413a50fc8d403498b3f0ded1
MMcClure11 <mcclme02@luther.edu>
2021-02-04 11:50:11 -0600

    Add a testUtils to import data-test attribute which will be reused for finding jsx elements

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Enzyme, { shallow, ShallowWrapper } from 'enzyme';
 import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 
+import { findByTestAttr } from '../test/testUtils'
 import Congrats from './Congrats';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
