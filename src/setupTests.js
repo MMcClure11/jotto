e1aad0440dc2c287e4c251ec5f942c57310d96d9
MMcClure11 <mcclme02@luther.edu>
2021-02-04 14:59:55 -0600

    Fix order of imports for setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -5,6 +5,6 @@
 import '@testing-library/jest-dom';
 
 import Enzyme from 'enzyme';
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
-import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
