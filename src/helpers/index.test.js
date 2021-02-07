adaaa8ec9ed1ec359209a513c80f839b0d3fe171
MMcClure11 <mcclme02@luther.edu>
2021-02-07 11:20:14 -0600

    Add expects for tests for getLetterMatchCount

--- src/helpers/index.test.js
+++ src/helpers/index.test.js
@@ -1,13 +1,17 @@
 import { getLetterMatchCount } from './';
 
 describe('getLettMatchCount', () => {
+  const secretWord = 'party';
   test('returns correct count when there are no matching letters', () => {
-
+    const letterMatchCount = getLetterMatchCount('bones', secretWord);
+    expoect(letterMatchCount).toBe(0);
   });
   test('returns the correct count when there are 3 matching letters', () => {
-
+    const letterMatchCount = getLetterMatchCount('trains', secretWord);
+    expect(letterMatchCount).toBe(3);
   });
   test('returns the correct count when there are dulicate letters in the guess', () => {
-
+    const letterMatchCount = getLetterMatchCount('parka', secretWord);
+    expect(letterMatchCount).toBe(3);
   });
 });
