428d09bf06cdc0677381f50f815bd2c2a3a01dd6
MMcClure11 <mcclme02@luther.edu>
2021-02-07 11:16:46 -0600

    Add outline of tests for getLetterMatchCount

---
+++ src/helpers/index.test.js
@@ -0,0 +1,13 @@
+import { getLetterMatchCount } from './';
+
+describe('getLettMatchCount', () => {
+  test('returns correct count when there are no matching letters', () => {
+
+  });
+  test('returns the correct count when there are 3 matching letters', () => {
+
+  });
+  test('returns the correct count when there are dulicate letters in the guess', () => {
+
+  });
+});
