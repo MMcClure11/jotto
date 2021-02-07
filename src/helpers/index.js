856fc51ab19288a60abc7462529b262df841deef
MMcClure11 <mcclme02@luther.edu>
2021-02-07 11:13:38 -0600

    Stub out index for getMatchedLetterCount

---
+++ src/helpers/index.js
@@ -0,0 +1,10 @@
+/**
+ * @method getLetterMatchCount
+ * @param {string} guessedWord - Guessed word. 
+ * @param {string} secretWord - Secret word.
+ * @returns {number} - Number of letters matched between guessed word and secret word.
+ */
+
+export function getLetterMatchCount(guessedWord, secretWord) {
+
+};
