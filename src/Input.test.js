625c5ff43e4ab985f4c8cf8bcf2b4f058cdad41e
MMcClure11 <mcclme02@luther.edu>
2021-02-08 18:38:14 -0600

    Set up Input component and storeFactory test util

---
+++ src/Input.test.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { findByTestAttr, storeFactory } from '../test/testUtils';
+import Input from './Input';
+
+/**
+ * Factory function to crete a ShallowWrapper for the Input component.
+ * @function setup
+ * @param {object} initialState - Initial state for this setup. 
+ * @returns {ShallowWrapper}
+ */
+
+const setup = (initialState={}) => {
+  const store = storeFactory(initialState);
+  const wrapper = shallow(<Input store={ store } />).dive().dive();
+  // console.log(wrapper.debug());//Higher Order Component from the connection that redux provides
+  return wrapper
+}
+
+// setup();
+
+describe('render', () => {
+  describe('word has not been guessed', () => {
+    test('renders component without error', () => {
+
+    });
+    test('renders input box', () => {
+
+    });
+    test('renders submit button', () => {
+
+    });
+  });
+  describe('word has been guessed', () => {
+    test('renders component without error', () => {
+
+    });
+    test('does not render input box', () => {
+
+    });
+    test('does not render submit button', () => {
+
+    });
+  });
+});
+describe('update state', () => {
+
+});
