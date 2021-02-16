274fb77617166d6e687594e032c00ab08ab64b37
MMcClure11 <mcclme02@luther.edu>
2021-02-16 11:58:39 -0600

    Add guessWord action creator on click of submit button

--- src/Input.test.js
+++ src/Input.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import { findByTestAttr, storeFactory } from '../test/testUtils';
-import Input from './Input';
+import Input, { UnconnectedInput } from './Input';
 
 /**
  * Factory function to crete a ShallowWrapper for the Input component.
@@ -71,5 +71,27 @@ describe('redux props', () => {
     const wrapper = setup();
     const guessWordProp = wrapper.instance().props.guessWord;
     expect(guessWordProp).toBeInstanceOf(Function);
+  });
+});
+
+describe('`guessWord` action creator call', () => {
+  test('calls `guessWord` when button is clicked', () => {
+    const guessWordMock = jest.fn();
+
+    const props = {
+      guessWord: guessWordMock,
+    };
+  
+    // set up input component with guessWordMock as the guessWord prop
+    const wrapper = shallow(<UnconnectedInput {...props} />);
+
+    // simluate clicked
+    const submitButton = findByTestAttr(wrapper, "submit-button");
+    submitButton.simulate('click');
+
+    // check to see if mock ran
+    const guessWordCallCount = guessWordMock.mock.calls.length;
+
+    expect(guessWordCallCount).toBe(1);
   });
 });
