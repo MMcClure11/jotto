0c49a6cfe7f8d76d62e28f50fe7b5c3f3d008e51
MMcClure11 <mcclme02@luther.edu>
2021-02-16 11:40:37 -0600

    Add getSecretWord action creator on App mount

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import { storeFactory } from '../test/testUtils';
-import App from './App';
+import App, { UnconnectedApp } from './App';
 
 /**
  * @function setup
@@ -42,3 +42,23 @@ describe('redux properities', () => {
   });
 });
 
+test('`getSecretWord` runs on App Mount', () => {
+  const getSecretWordMock = jest.fn();
+
+  const props = {
+    getSecretWord: getSecretWordMock,
+    success: false,
+    guessedWords: [],
+  }
+  // set up app component with getSecretWordMock as the getSecretWord prop
+  const wrapper = shallow(<UnconnectedApp {...props} />);
+
+  //run lifecycle method
+  wrapper.instance().componentDidMount();
+
+  //check to see if mock ran
+  const getSecretWordCallCount = getSecretWordMock.mock.calls.length;
+
+  expect(getSecretWordCallCount).toBe(1);
+
+});
