f35500fa214a21146511568731507118dea4305b
MMcClure11 <mcclme02@luther.edu>
2021-02-18 12:31:09 -0600

    Add test for resetAction called on button click

--- src/NewWordButton.test.js
+++ src/NewWordButton.test.js
@@ -32,3 +32,15 @@ test('does not thow warning with expeced props', () => {
   const expectedProps = { display: false, resetAction: () => {} };
   checkProps(NewWordButton, expectedProps);
 });
+test('calls `resetAction` prop upon button click', () => {
+  //create a mock funciton so we can see whether it's called on click
+  const resetActionMock = jest.fn();
+  const wrapper = setup({ display: true, resetAction: resetActionMock });
+
+  //find the button (which is the top level element of this component)
+  const resetButton = findByTestAttr(wrapper, 'component-new-word-button');
+  resetButton.simulate('click');
+
+  //expect the mock to have been called once
+  expect(resetActionMock.mock.calls.length).toBe(1);
+});
